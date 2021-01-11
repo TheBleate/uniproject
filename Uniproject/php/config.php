7fbf5d749aad624ea2936ffdfa99094d0bae60d3
foket19 <andihalauca@gmail.com>
2021-01-11 17:28:59 +0100

    Falta configurar
    Co-Authored-By: foket19 <39842199+foket19@users.noreply.github.com>

---
+++ Uniproject/php/config.php
@@ -0,0 +1,13 @@
+<?php
+/* +------------------------------------+
+   |       FITXER DE CONFIGURACIÓ       |
+   +------------------------------------+ */
+
+   //ACCESS A LA BASE DE DADES
+   const DB_USER = 'root';
+   const DB_PASS = 'Alumne1234';
+   const DB_NAME = 'Sprint2';
+   const DB_ADDRESS = '192.168.224.115';
+   const DB_PORT = 3306;
+
+?>
