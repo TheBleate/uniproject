d06aee49587e57e4e8de22b6a756a0c3271d5937
foket19 <andihalauca@gmail.com>
2021-01-13 19:57:38 +0100

    juntar php

--- Uniproject/php/institut.php
+++ Uniproject/php/institut.php
@@ -1,9 +1,8 @@
 <?php
 class Institut {
   private $DB;
-  public function altaInstitut($idinstitut, $nom, $localitat, $adreça, $telefon, $cif, $email) {
+  public function alta($idinstitut, $nom, $localitat, $adreça, $telefon, $cif, $email) {
     $query = 'INSERT INTO Institut(idInstitut, Nom, Localitat, Adreça, Telefon, CIF, Email) VALUES '. "(". '"' . $idinstitut . '","' . $nom . '","' . $localitat .  '","' . $adreça .  '","' . $telefon . '","' . $cif . '","' . $email .'");';
-
     $query = $this->DB->query($query);
     if ($query === true) {
           return "Empresa afegida!";
@@ -11,8 +10,52 @@ class Institut {
           return "No s'han pogut afegir la Empresa!";
       }
     }
+    function eliminar($idInstitut) {
+        $query = 'UPDATE Institut SET Estat = "inactiu" WHERE idInstitut = "' . $idInstitut. '"';
+        $query = $this->DB->query($query);
+        if ($query === true) {
+            return "L'institut s'ha eliminat!";
+        } else {
+            return "No s'ha pogut eliminar l'institut!";
+        }
+    }
+    public function llistar() {
+      $query = 'SELECT nom, direccio, localitat, Estat FROM Usuari WHERE Tipus = "institut";';
+      $query = $this->DB->query($query);
+      if ($query) {
+          echo '<table border="1" frame="void" rules="cols">
+                  <thead>
+                      <tr>
+                          <th>nom</th>
+                          <th>direccio</th>
+                          <th>localitat</th>
+                          <!--  <th>Estat</th> -->
+                          <td><a href="./crudusuaris.php?action=0&type=0"><i class="fas fa-plus"></i></a></td>
+                      </tr>
+                  </thead>
+                  <tbody>';
+          foreach ($query as $row) {
+            if($row['Estat'] === 'actiu') {
+                echo '<tr>
+                        <td>'. $row['nom'] .'</td>
+                        <td>'. $row['direccio'] .'</td>
+                        <td>'. $row['localitat'] .'</td>
+                        <!--  <td>'. $row['Estat'] .'</td> -->
+                        <td>
+                            <div class="table-icons">
+                                <a href="./crudusuaris.php?uid='. $row['idUsuari']. '&action=1&type=0"><i class="fas fa-edit"></i></a>
+                                <a href="./crudusuaris.php?uid='. $row['idUsuari']. '&action=2&type=0"><i class="fas fa-trash-alt"></i></a>
+                            <div>
+                        </td>
+                    </tr>';
+            }
+          }
+          echo '    </tbody>
+                </table>';
+      }
+  }
   function __construct($DB) {
       $this->DB = $DB;
   }
 }
- ?>
+?>
