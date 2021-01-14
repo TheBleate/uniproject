62a8aa518b90729470881973af151341182da313
foket19 <andihalauca@gmail.com>
2021-01-14 21:51:21 +0100

    c

--- Uniproject/login.php
+++ Uniproject/login.php
@@ -10,21 +10,16 @@
     <?php $tab=0; $navbar=0; require_once('./template/navbar.php') ?>
 
 	<?php
-
 		require_once('./php/database.php');
-
 		$nomusuari = $_POST['user'] ?? null;
 		$contraseña = $_POST['pass'] ?? null;
-
 		//Creem un nou objecte de Database
 		$DB = new Database();
-
 		//array_key_exists Comprova que dintre de Get existeix el valor
 		if (array_key_exists('logout', $_GET)) {
 			session_destroy();
 			unset($_SESSION['current-user']);
 		}
-
 		//$_SESSION Guarda el usuari de la sessió, en cas de no tindre una sessió, serà null
 		//la variable guarda la sessió al servidor fins que és tanca la sessio
 		$usuariactual = $_SESSION['current-user'] ?? null;
