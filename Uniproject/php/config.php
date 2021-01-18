b2c95bdbf11096877e04392814251b0517c6a3fb
walraymmo <walraymmo@gmail.com>
2021-01-18 18:13:56 +0100

    ...

--- Uniproject/php/config.php
+++ Uniproject/php/config.php
@@ -10,13 +10,6 @@
    define('DB_NAME', 'proiectus');
    define('DB_ADDRESS', '127.0.0.1');
    define('DB_PORT', '3306');
-   /*
-   define('DB_USER', 'root');
-   define('DB_PASS', '1234');
-   define('DB_NAME', 'Proiectus');
-   define('DB_ADDRESS', '127.0.0.1');
-   define('DB_PORT', '3306');
-   */
 
    //MODE DEPURACIÓ
    //define('DEBUG_MODE' true)
