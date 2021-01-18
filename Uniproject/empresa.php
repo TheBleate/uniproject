396947755a68faf86d184d42e2a8be49027a6dd5
marcesp15 <marcespana2@iesmontsia.org>
2021-01-18 17:04:21 +0100

    commit

--- Uniproject/empresa.php
+++ Uniproject/empresa.php
@@ -58,7 +58,7 @@
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Creacio d'instituts</h5>
+                <h5 class="modal-title" id="staticBackdropLabel">Creacio d'empreses</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
