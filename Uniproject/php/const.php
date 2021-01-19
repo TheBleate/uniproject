e3ed6d5cb712960f6ebce6df9b308c26915d6384
walraymmo <walraymmo@gmail.com>
2021-01-19 17:37:38 +0100

    ...

--- Uniproject/php/const.php
+++ Uniproject/php/const.php
@@ -8,17 +8,10 @@
     define('ROOT_PATH', realpath(__DIR__. '/..'). '/');
 
     // Ruta absoluta del directori CSS
-<<<<<<< HEAD
-    define('STYLES_PATH', (is_callable('comparePath') ? comparePath(ROOT_PATH, $_SERVER["PHP_SELF"]) : '/'). 'css/');
-
-    // Ruta absoluta del directori JavaScript
-    define('SCRIPTS_PATH', (is_callable('comparePath') ? comparePath(ROOT_PATH, $_SERVER["PHP_SELF"]) : '/'). 'js/');
-=======
     define('STYLES_PATH', (is_callable('comparePath') ? comparePath(ROOT_PATH, $_SERVER["PHP_SELF"]) : './'). 'css/');
 
     // Ruta absoluta del directori JavaScript
     define('SCRIPTS_PATH', (is_callable('comparePath') ? comparePath(ROOT_PATH, $_SERVER["PHP_SELF"]) : './'). 'js/');
->>>>>>> ee4efae45b4881a9a1e43cfb6d8096d7f0bc4c8b
 
     // Ruta absoluta del directori Views utilitzat per a la plantilla
     define('VIEWS_PATH', ROOT_PATH. 'views/');
