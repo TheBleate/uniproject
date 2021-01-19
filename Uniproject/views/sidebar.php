cfdaa25ceb467ec65a0b214a20467c831eb99bca
foket19 <andihalauca@gmail.com>
2021-01-19 19:34:00 +0100

    c

--- Uniproject/views/sidebar.php
+++ Uniproject/views/sidebar.php
@@ -4,22 +4,14 @@ switch ($args[0] ?? 0):
   case 0:?>
   <!-- Sidebar -->
   <div class="bg-dark">
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="list-group">
 
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item <?php echo(CURRENT_FILENAME === 'gestor' ? 'active' : ''); ?>">
-            <a class="nav-link" href="../gestor.php">Usuaris</a>
-          </li>
-          <li class="nav-item <?php echo(CURRENT_FILENAME === 'institut'  ? 'active' : ''); ?>">
-            <a class="nav-link" href="../institut.php">Instituts</a>
-          </li>
-          <li class="nav-item <?php echo(CURRENT_FILENAME === 'categoria'  ? 'active' : ''); ?>">
-            <a class="nav-link" href="./categoria.php"> Categoria</a>
-          </li>
-          <li class="nav-item <?php echo(CURRENT_FILENAME === 'Incidencia'  ? 'active' : ''); ?>">
-            <a class="nav-link" href="../incidencia.php"> Incidencies</a>
-          </li>
-        </ul>
+      <a href="./gestor.php" class="list-group-item list-group-item-action <?php echo(CURRENT_FILENAME === 'gestor' ? 'active' : ''); ?>">Usuaris</a>
+      <a href="institut.php" class="list-group-item list-group-item-action <?php echo(CURRENT_FILENAME === 'institut'  ? 'active' : ''); ?>">Instituts</a>
+      <a href="./empresa.php" class="list-group-item list-group-item-action <?php echo(CURRENT_FILENAME === 'empresa'  ? 'active' : ''); ?>">Empresa</a>
+      <a href="./categoria.php" class="list-group-item list-group-item-action <?php echo(CURRENT_FILENAME === 'categoria'  ? 'active' : ''); ?>">Categoria</a>
+      <a href="./incidencia.php" class="list-group-item list-group-item-action <?php echo(CURRENT_FILENAME === 'incidencia'  ? 'active' : ''); ?>">Incidencia</a>
+
     </div>
   </div>
 <?php break;
@@ -28,3 +20,18 @@ switch ($args[0] ?? 0):
 
 <?php break;
   endswitch; ?>
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item ">
+      <a class="nav-link" href="./index.php">Usuaris</a>
+    </li>
+
+    <li class="nav-item <?php echo(CURRENT_FILENAME === 'institut'  ? 'active' : ''); ?>">
+      <a class="nav-link" href="./about.php">Instituts</a>
+    </li>
+    <li class="nav-item <?php echo(CURRENT_FILENAME === 'categoria'  ? 'active' : ''); ?>">
+      <a class="nav-link" href="./contact.php"> Categoria</a>
+    </li>
+    <li class="nav-item <?php echo(CURRENT_FILENAME === 'Incidencia'  ? 'active' : ''); ?>">
+      <a class="nav-link" href="./contact.php"> Incidencies</a>
+    </li>
+  </ul>
