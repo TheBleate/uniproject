ef633582ddcfc2555799f279cfddd2d95a4ee6e1
foket19 <andihalauca@gmail.com>
2021-01-19 19:26:37 +0100

    sd

--- Uniproject/views/sidebar.php
+++ Uniproject/views/sidebar.php
@@ -4,20 +4,20 @@ switch ($args[0] ?? 0):
   case 0:?>
   <!-- Sidebar -->
   <div class="bg-dark">
-    <div class="list-group">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
         <ul class="navbar-nav mr-auto">
           <li class="nav-item <?php echo(CURRENT_FILENAME === 'gestor' ? 'active' : ''); ?>">
-            <a class="nav-link" href="./gestor.php">Usuaris</a>
+            <a class="nav-link" href="../gestor.php">Usuaris</a>
           </li>
-
           <li class="nav-item <?php echo(CURRENT_FILENAME === 'institut'  ? 'active' : ''); ?>">
-            <a class="nav-link" href="./institut.php">Instituts</a>
+            <a class="nav-link" href="../institut.php">Instituts</a>
           </li>
           <li class="nav-item <?php echo(CURRENT_FILENAME === 'categoria'  ? 'active' : ''); ?>">
             <a class="nav-link" href="./categoria.php"> Categoria</a>
           </li>
           <li class="nav-item <?php echo(CURRENT_FILENAME === 'Incidencia'  ? 'active' : ''); ?>">
-            <a class="nav-link" href="./incidencia.php"> Incidencies</a>
+            <a class="nav-link" href="../incidencia.php"> Incidencies</a>
           </li>
         </ul>
     </div>
