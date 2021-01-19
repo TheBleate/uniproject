891ec00e626bfdc5db33e50c582104324438800d
foket19 <andihalauca@gmail.com>
2021-01-19 19:13:07 +0100

    c

--- Uniproject/views/sidebar.php
+++ Uniproject/views/sidebar.php
@@ -5,10 +5,21 @@ switch ($args[0] ?? 0):
   <!-- Sidebar -->
   <div class="bg-dark">
     <div class="list-group">
-      <a href="./gestor.php" class="list-group-item list-group-item-action active">Usuaris</a>
-      <a href="institut.php" class="list-group-item list-group-item-action">Instituts</a>
-      <a href="" class="list-group-item list-group-item-action">Categoria</a>
-      <a href="./incidencia.php" class="list-group-item list-group-item-action disabled">Incidencies</a>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item <?php echo(CURRENT_FILENAME === 'gestor' ? 'active' : ''); ?>">
+            <a class="nav-link" href="./gestor.php">Usuaris</a>
+          </li>
+
+          <li class="nav-item <?php echo(CURRENT_FILENAME === 'institut'  ? 'active' : ''); ?>">
+            <a class="nav-link" href="./institut.php">Instituts</a>
+          </li>
+          <li class="nav-item <?php echo(CURRENT_FILENAME === 'categoria'  ? 'active' : ''); ?>">
+            <a class="nav-link" href="./categoria.php"> Categoria</a>
+          </li>
+          <li class="nav-item <?php echo(CURRENT_FILENAME === 'Incidencia'  ? 'active' : ''); ?>">
+            <a class="nav-link" href="./incidencia.php"> Incidencies</a>
+          </li>
+        </ul>
     </div>
   </div>
 <?php break;
@@ -17,18 +28,3 @@ switch ($args[0] ?? 0):
 
 <?php break;
   endswitch; ?>
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item <?php echo(CURRENT_FILENAME === 'gestor' ? 'active' : ''); ?>">
-      <a class="nav-link" href="./gestor.php">Usuaris</a>
-    </li>
-
-    <li class="nav-item <?php echo(CURRENT_FILENAME === 'institut'  ? 'active' : ''); ?>">
-      <a class="nav-link" href="./institut.php">Instituts</a>
-    </li>
-    <li class="nav-item <?php echo(CURRENT_FILENAME === 'categoria'  ? 'active' : ''); ?>">
-      <a class="nav-link" href="./categoria.php"> Categoria</a>
-    </li>
-    <li class="nav-item <?php echo(CURRENT_FILENAME === 'Incidencia'  ? 'active' : ''); ?>">
-      <a class="nav-link" href="./incidencia.php"> Incidencies</a>
-    </li>
-  </ul>
