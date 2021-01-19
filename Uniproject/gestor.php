81a879467e4373e8c483f5ba64d03ed813344932
foket19 <andihalauca@gmail.com>
2021-01-19 20:10:48 +0100

    c

--- Uniproject/gestor.php
+++ Uniproject/gestor.php
@@ -59,10 +59,13 @@
                 $telefon = (isset($_POST['$telefon']) ? $_POST['$telefon'] : null);
                 $dataNaixement = (isset($_POST['$DataNaixement']) ? $_POST['$dataNaixement'] : null);
             ?>
+<<<<<<< HEAD
         <!-- Modal button (se cambiara)
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-creacio">
           Crear (icona)
         </button> -->
+=======
+>>>>>>> edb2a78b03bd4ab757c677eaf46dc71260aa65ce
         <!-- Modal de Creacio -->
         <div class="modal fade" id="modal-creacio" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered">
