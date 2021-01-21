12ebcab739625368ed425c86b6df8c8dfc130066
walraymmo <walraymmo@gmail.com>
2021-01-21 01:54:51 +0100

    ...

--- Uniproject/php/controller.php
+++ Uniproject/php/controller.php
@@ -3,7 +3,35 @@
    |        FITXER INTERMEDIARI         |
    +------------------------------------+ */
 
+   require_once('./const.php');
+   require_once('./database.php');
+   require_once('./categoria.php');
 
 
-    echo("Asdasdsda");
+
+   $tab = $_POST['tab'] ?? null;
+   $action = $_POST['action'] ?? null;
+   $referrer = null;
+
+   if ($tab && $action) {
+       $DB = new Database();
+       switch ($tab) {
+          case 'categoria':
+            $GC = new Categoria($DB->getDB());
+            switch($action) {
+                case 'alta':
+                    $nom = $_POST['nom'] ?? null;
+                    $descripcio = $_POST['descripcio'] ?? null;
+
+                    if ($nom && $descripcio) {
+                        $GC->alta($nom, $descripcio);
+                    }
+                    break;
+
+            }
+            break;
+       }
+   }
+
+   exit('Error inesperat');
 ?>
